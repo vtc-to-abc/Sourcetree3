16ce666963792b69dde7adb9b45d8e71299cd02e
vtc-to-abc <vuthanhcong31998@gmail.com>
2022-04-22 16:42:35 +0700

    1st client size commit

---
+++ BlazorClient/Pages/Book/BookEditModal.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using BlazorClient.Models;
+using BlazorClient.Services;
+using System.Web.Mvc;
+
+namespace BlazorClient.Pages.Book
+{
+    public partial class BookEditModal
+    {
+        [Inject] private IBookService BookService { get; set; }
+        [Inject] private IAuthorService AuthorService { get; set; }
+        [Inject] private IAuthorBookService AuthorBookService { get; set; }
+        [Parameter] public BookModel Book { get; set; }
+        [Parameter]public IEnumerable<AuthorModel> WritenByAuthors { get; set; }
+        private bool OpenModal = false;
+        public IEnumerable<BookModel> Books { get; set; } 
+        protected override async Task OnInitializedAsync()
+        {
+
+            await base.OnInitializedAsync();
+        }
+
+
+        public void EditCurrentData()
+        {
+            
+             BookService.EditBook(Book);
+            StateHasChanged();
+
+        }
+
+
+        public void SetVisible(bool visible)
+        {
+            OpenModal = visible;
+            StateHasChanged();
+        }
+
+    }
+}
--- BlazorClient/Program.cs
+++ BlazorClient/Program.cs
@@ -3,20 +3,34 @@ using BlazorClient.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using System.Net.Http.Headers;
+using MudBlazor.Services;
+using Blazored.Modal;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddMudServices();
+builder.Services.AddBlazoredModal();
+/// <summary>
+///    The request-header fields allow the client to pass additional
+///    information about the request, and about the client itself, to the
+///    server. These fields act as request modifiers, with semantics
+///    equivalent to the parameters on a programming language method
+///    invocation.
+/// </summary>
+/// set up client side
 builder.Services.AddScoped(sp => {
     var httpClient = new HttpClient { BaseAddress = new Uri("https://localhost:7051") };
-    httpClient.DefaultRequestHeaders
-      .Accept
-      .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    httpClient.DefaultRequestHeaders// request header
+      .Accept // what server reponse type will this side's request accept.
+      .Add(new MediaTypeWithQualityHeaderValue("application/json")); // this side accept "application/json" response type
     return httpClient;
     });
 
 builder.Services.AddTransient<IAuthorService, AuthorService>();
+builder.Services.AddTransient<IBookService, BookService>();
+builder.Services.AddTransient<IAuthorBookService, AuthorBookService>();
+
 var app = builder.Build();
 
 await app.RunAsync();
